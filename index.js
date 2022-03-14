e287f864de84916a835d3f8f2048556ccd91eb97
manishfw13 <manish_fw13_347@masai.school>
2022-03-14 12:48:40 +0530

    user completed

---
+++ index.js
@@ -0,0 +1,181 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+app.use(express.json())
+
+
+const connect = () => {
+    return mongoose.connect("mongodb://127.0.0.1:27017/EVALUATION")
+}
+
+  
+const userSchema = new mongoose.Schema(
+    {
+      firstName: { type: String, required: true },
+      middleName: { type: String, required: false },
+      lastName: { type: String, required: false },
+      age:{type:Number, required:true},
+      email: { type: String, required: true },
+      address: { type: String, required: true },
+      gender: { type: String, required: true },
+     
+    },
+    {
+      versionKey: false,
+      timestamps: true, // createdAt, updatedAt
+    }
+  );
+  
+ 
+  const User = mongoose.model("evalData", userSchema); 
+
+  
+  const branchSchemas = new mongoose.Schema(
+    {
+    id: { type: Number, required: true },
+    name: { type: String, required: true },
+    adress: { type: String, required: true },
+    IFSC: { type: String, required: true },
+    MICR: { type: Number, required: true },
+
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "evalData",
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+ 
+const Branch = mongoose.model("branch_details", branchSchemas);
+
+const MasterSchemas = new mongoose.Schema(
+    {
+      id: { type: Number, required: true },
+      balance: { type: Number, required: true },
+      branchinfo_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "branch_details",
+        required: true,
+      },
+    },
+    {
+      versionKey: false,
+      timestamps: true,
+    }
+  );
+ 
+  const master_account = mongoose.model("master", MasterSchemas);
+
+  const savingSchemas = new mongoose.Schema(
+    {
+      id: { type: Number, required: true },
+      accountnumber: { type: Number, required: true, unique: true },
+      balance: { type: Number, required: true },
+      intrestRate: { type: Number, required: true },
+      master_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "master",
+        required: true,
+      },
+    },
+    {
+      versionKey: false,
+      timestamps: true,
+    }
+  );
+  
+ 
+  const Saving_account = mongoose.model("saving", savingSchemas);
+
+
+  
+  const fixedSchemas = new mongoose.Schema(
+    {
+      id: { type: Number, required: true },
+      accountnumber: { type: Number, required: true, unique: true },
+      balance: { type: Number, required: true },
+      intrestRate: { type: Number, required: true },
+      startDate: { type: Number, required: true },
+      maturityDate: { type: Number, required: true },
+      savingac_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "saving",
+        required: true,
+      },
+    },
+    {
+      versionKey: false,
+      timestamps: true,
+    }
+  );
+  
+  
+  const fixed_account = mongoose.model("fixed", fixedSchemas);
+  
+
+
+  
+
+
+  app.get("/user", async (res, req) => {
+    try {
+        const users = await User.find().lean().exec();
+        return res.statu(201).send(users)
+    }
+    catch(e) {
+        return res.statusCode(500).send(e.message)
+    }
+})
+
+app.post("/user", async (res, req) => {
+    try {
+        const users = await User.create(req.body);
+        return res.statu(201).send(users)
+    }
+    catch(e) {
+        return res.statusCode(500).send(e.message)
+    }
+})
+
+app.patch("/user/:id", async (res, req) => {
+    try {
+        const users = await User.findByIdAndDelete(req.params.id, req.body,
+            {
+                new: true
+            }
+        ).lean().exec();
+        return res.statu(201).send(users)
+    }
+    catch(e) {
+        return res.statusCode(500).send(e.message)
+    }
+})
+
+app.delete("/user/:id", async (res, req) => {
+    try {
+        const users = await User.deleteMany({balance:0}).lean().exec();
+        return res.statu(201).send(users)
+    }
+    catch(e) {
+        return res.statusCode(500).send(e.message)
+    }
+})
+
+
+
+app.listen(2000, async () => {
+    try {
+      await connect();
+    } catch (err) {
+      console.log(err);
+    }
+  
+    console.log("listening on port 2000");
+  });
+  
